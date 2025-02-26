aad0e28519fdecce49de5722429db02db2850c18
PeytonCVT <peyton.ellis@cvtechonline.net>
2025-02-26 13:18:24 -0600

    Delete Javascript directory

--- Javascript/CHECK - Javascript Exponents/index.js
+++
@@ -1,6 +0,0 @@
-function check() {
-var num = Number(document.getElementById("number").value);
-var exp = Number(document.getElementById("exponent").value);
-var equal = (num ** exp)
-alert(equal)
-}
